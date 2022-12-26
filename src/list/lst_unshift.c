6c2a3fce50dcab875858d9fd4796ac19be164612
tomatte <daniel.brandao2000@gmail.com>
2022-12-26 14:45:21 -0300

    changed lst_unshift to work better with null pointers

--- src/list/lst_unshift.c
+++ src/list/lst_unshift.c
@@ -6,18 +6,23 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 12:39:59 by dbrandao          #+#    #+#             */
-/*   Updated: 2022/12/24 12:59:49 by dbrandao         ###   ########.fr       */
+/*   Updated: 2022/12/26 14:42:11 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
 //add element to beginning of list
-void	lst_unshift(t_lst *lst, t_lst *node)
+void	lst_unshift(t_lst **lst, t_lst *node)
 {
 	if (!lst || !node)
 		return ;
-	lst = lst_first(lst);
-	lst->prev = node;
-	node->next = lst;
+	if (!*lst)
+	{
+		*lst = node;
+		return ;
+	}
+	*lst = lst_first(*lst);
+	(*lst)->prev = node;
+	node->next = *lst;
 }
--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 13:22:27 by dbrandao          #+#    #+#             */
-/*   Updated: 2022/12/26 14:38:31 by dbrandao         ###   ########.fr       */
+/*   Updated: 2022/12/26 14:43:13 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ typedef struct	s_lst
 t_lst	*lst_new(int num);
 int		lst_size(t_lst *lst);
 t_lst	*lst_first(t_lst *lst);
-void	lst_unshift(t_lst *lst, t_lst *node);
+void	lst_unshift(t_lst **lst, t_lst *node);
 t_lst	*lst_last(t_lst *lst);
 void	lst_push(t_lst **lst, t_lst *node);
 t_lst	*lst_shift(t_lst *lst);
