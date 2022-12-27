d09f1aa567ddb8331d1583911c9833aa8e240f4e
tomatte <daniel.brandao2000@gmail.com>
2022-12-27 10:44:57 -0300

    improved lst_shift

--- src/list/lst_shift.c
+++ src/list/lst_shift.c
@@ -6,19 +6,22 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 19:17:08 by dbrandao          #+#    #+#             */
-/*   Updated: 2022/12/24 19:22:10 by dbrandao         ###   ########.fr       */
+/*   Updated: 2022/12/27 10:41:36 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-t_lst	*lst_shift(t_lst *lst)
+t_lst	*lst_shift(t_lst **lst)
 {
 	t_lst	*node;
 
-	node = lst_first(lst);
-	lst = node->next;
+	if (!lst || !*lst)
+		return (NULL);
+	node = lst_first(*lst);
+	*lst = node->next;
+	if (*lst)
+		(*lst)->prev = NULL;
 	node->next = NULL;
-	lst->prev = NULL;
 	return (node);
 }
