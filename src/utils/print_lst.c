c37233d46bab265f21dd7bc5d89bc26298476cdf
tomatte <daniel.brandao2000@gmail.com>
2023-01-07 08:25:40 -0300

    print_lst

---
+++ src/utils/print_lst.c
@@ -0,0 +1,14 @@
+#include "../push_swap.h"
+
+void	print_lst(t_lst *lst)
+{
+	if (!lst)
+		ft_printf("Empty List\n");
+	lst = lst_first(lst);
+	while (lst)
+	{
+		ft_printf("%d\t(%d)\n", lst->num, lst->position);
+		lst = lst->next;
+	}
+	ft_printf("\n");
+}
