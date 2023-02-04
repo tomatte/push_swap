8f13249478eaf5a031e4a1ecc67fea3d8206cbf1
tomatte <daniel.brandao2000@gmail.com>
2023-02-04 11:07:17 -0300

    modify three_solver to work with any indexes

--- src/algo/three_solver.c
+++ src/algo/three_solver.c
@@ -6,19 +6,37 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 08:35:10 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/02/04 09:21:27 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/02/04 11:02:12 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
+static int	get_biggest_index(t_lst *a)
+{
+	int	biggest_index;
+
+	biggest_index = a->index;
+	while (a->next)
+	{
+		a = a->next;
+		if (a->index > biggest_index)
+			biggest_index = a->index;
+	}
+	return (biggest_index);
+}
+
 void	three_solver(t_lst *a)
 {
+	int	biggest;
+
 	a = lst_first(a);
-	if (a->index == 2)
+	biggest = get_biggest_index(a);
+	if (a->index == biggest)
 		ra(&a);
-	else if (a->next->index == 2)
+	else if (a->next->index == biggest)
 		rra(&a);
-	if (!is_lst_sorted(a))
+	a = lst_first(a);
+	if (a->index > a->next->index)
 		sa(&a);
 }
