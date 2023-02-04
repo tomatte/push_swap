0924686269cdfbde3f95e878373ff5532307ecbc
tomatte <daniel.brandao2000@gmail.com>
2023-02-04 11:23:00 -0300

    add aux function to push to b

--- src/algo/push_rotate/push_rotate.c
+++ src/algo/push_rotate/push_rotate.c
@@ -6,15 +6,24 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 10:47:54 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/02/04 11:10:23 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/02/04 11:21:50 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
-static void	push_to_b()
+static void	push_to_b(t_lst **a, t_lst **b)
 {
+	int	size_b;
+	int	i;
 
+	size_b = lst_size(*a) - 3;
+	i = 0;
+	while (i < size_b)
+	{
+		pb(b, a);
+		i++;
+	}
 }
 
 void	push_rotate(t_lst *a)
@@ -23,6 +32,12 @@ void	push_rotate(t_lst *a)
 
 	b = NULL;
 	push_to_b(&a, &b);
-	three_solver(a);
-	push_rotate_algorithm(&a, &b);
+
+	ft_printf("A:\n");
+	print_lst(a);
+	ft_printf("B:\n");
+	print_lst(b);
+
+/* 	three_solver(a);
+	push_rotate_algorithm(&a, &b); */
 }
