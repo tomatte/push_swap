5d1581fdbf7290e837b5696abaabfec1318e2c82
tomatte <daniel.brandao2000@gmail.com>
2023-02-04 09:23:34 -0300

    make smaller three_solver function

--- src/algo/three_solver.c
+++ src/algo/three_solver.c
@@ -6,7 +6,7 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 08:35:10 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/01/23 22:47:55 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/02/04 09:21:27 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,10 @@
 void	three_solver(t_lst *a)
 {
 	a = lst_first(a);
-	if (a->index == 0 && a->next->index == 2)
-	{
-		rra(&a);
-		sa(&a);
-	}
-	else if (a->index == 1 && a->next->index == 0)
-		sa(&a);
-	else if (a->index == 1 && a->next->index == 2)
-		rra(&a);
-	else if (a->index == 2 && a->next->index == 0)
+	if (a->index == 2)
 		ra(&a);
-	else if (a->index == 2 && a->next->index == 1)
-	{
-		ra(&a);
+	else if (a->next->index == 2)
+		rra(&a);
+	if (!is_lst_sorted(a))
 		sa(&a);
-	}
 }
