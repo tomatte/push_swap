286d81114a45fc2d9877daa47ffd665297d97652
tomatte <daniel.brandao2000@gmail.com>
2023-02-09 14:51:02 -0300

    add execute_reverses
    last aux function of execute_strategy function that will rrr when
    possible

--- src/algo/push_rotate/execute_strategy.c
+++ src/algo/push_rotate/execute_strategy.c
@@ -6,7 +6,7 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:47:52 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/02/09 14:43:58 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/02/09 14:49:35 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	execute_updown(t_lst **a, t_lst **b, t_strategy *strategy)
 	move_to_top_b(b, strategy->updown_index);
 }
 
-void	execute_rotates(t_lst **a, t_lst **b, t_strategy *strategy)
+static void	execute_rotates(t_lst **a, t_lst **b, t_strategy *strategy)
 {
 	int	a_index;
 
@@ -35,9 +35,17 @@ void	execute_rotates(t_lst **a, t_lst **b, t_strategy *strategy)
 	move_to_top_b(b, strategy->updown_index);
 }
 
-void	execute_reverses(t_lst *a, t_lst *b, t_strategy *strategy)
+static void	execute_reverses(t_lst **a, t_lst **b, t_strategy *strategy)
 {
+	int	a_index;
 
+	a_index = get_top_index(*a, strategy->updown_index);
+	while ((*a)->index != a_index && (*b)->index != strategy->updown_index)
+	{
+		rrr(a, b);
+	}
+	move_to_top_a(a, a_index);
+	move_to_top_b(b, strategy->updown_index);
 }
 
 void	execute_strategy(t_lst **a, t_lst **b, t_strategy *strategy)
@@ -49,11 +57,11 @@ void	execute_strategy(t_lst **a, t_lst **b, t_strategy *strategy)
 	rotates = strategy->rotate_moves;
 	reverses = strategy->reverse_moves;
 	updowns = strategy->updown_moves;
-	//if (updowns <= reverses && updowns <= rotates)
-	//	execute_updown(a, b, strategy);
-	//else if (rotates <= reverses && rotates <= updowns)
-	execute_rotates(a, b, strategy);
-/* 	else
-		execute_reverses(); */
+	if (updowns <= reverses && updowns <= rotates)
+		execute_updown(a, b, strategy);
+	else if (rotates <= reverses && rotates <= updowns)
+		execute_rotates(a, b, strategy);
+	else
+		execute_reverses(a, b, strategy);
 	pa(a, b);
 }
